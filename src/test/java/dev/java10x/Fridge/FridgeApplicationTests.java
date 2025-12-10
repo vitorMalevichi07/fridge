ddd0b8e42ceec522a8da83ff4b8025480d8556de
Vitor Malevichi <vmalevichi@gmail.com>
2025-12-10 14:40:13 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/dev/java10x/Fridge/FridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
